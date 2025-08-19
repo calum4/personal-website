876cb372261e28c588ec6c0046dc88ac8cd4ec8a
Calum <calum@calumm.dev>
2025-08-19 13:52:29 +0100

    wip: terminal component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Terminal } from './terminal/terminal';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Terminal],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('personal-website');
 }
