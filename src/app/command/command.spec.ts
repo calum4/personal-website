78a52dd87e80cb325a8aab06209cd530d89aec6b
Calum <calum@calumm.dev>
2025-08-19 22:15:53 +0100

    test: ensure handled and help commands match

--- src/app/command/command.spec.ts
+++ src/app/command/command.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Command } from './command';
+import {COMMANDS} from './command.model';
+
+const SKIP_COMMANDS = ["clear", ""];
 
 describe('Command', () => {
   let component: Command;
@@ -20,4 +22,53 @@ describe('Command', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("template commands should handle all commands", async () => {
+    const templateCommands = await getHtmlTemplateCommands();
+
+    for (const command of COMMANDS) {
+      if (SKIP_COMMANDS.includes(command)) continue;
+
+      if (!templateCommands.includes(command)) {
+        throw new Error(`template commands does not contain command "${command}"`);
+      }
+    }
+
+    expect(true).toBeTrue();
+  });
+
+  it("commands should handle all template commands", async () => {
+    const templateCommands = await getHtmlTemplateCommands();
+
+    for (const command of templateCommands) {
+      if (SKIP_COMMANDS.includes(command)) continue;
+
+      if (!COMMANDS.includes(command)) {
+        throw new Error(`COMMANDS does not contain template command "${command}"`);
+      }
+    }
+
+    expect(true).toBeTrue();
+  });
 });
+
+async function getHtmlTemplateCommands(): Promise<string[]> {
+  const test = await fetch("/html/app/command/command.html");
+  const template = await test.text();
+
+  const startIndex = template.search("<!-- START COMMAND CHECK -->");
+  const endIndex = template.search("<!-- END COMMAND CHECK -->");
+
+  if (startIndex === -1 || endIndex === -1) {
+    throw new Error("Unable to find the COMMAND CHECK area");
+  }
+
+  const commandCheck = template.substring(startIndex, endIndex);
+  const templateCommands: string[] = [];
+
+  for (const match of commandCheck.matchAll(/(@case\s*\(["'])(.*)(["']\)\s*{)/g)) {
+    templateCommands.push(match[2]);
+  }
+
+  return templateCommands;
+}
