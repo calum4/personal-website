22bf11c4c90f521541e5d543ddad6e536fa67876
Calum <calum@calumm.dev>
2025-08-21 18:50:24 +0100

    feat: openssl use dsaparam

--- nginx/5-ssl.sh
+++ nginx/5-ssl.sh
@@ -5,7 +5,7 @@ set -e
 if [ ! -e .ssl-created ]
 then
   openssl req -x509 -nodes -days 5475 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -batch
-  openssl dhparam -out /etc/nginx/dhparam.pem 4096
+  openssl dhparam -dsaparam -out /etc/nginx/dhparam.pem 4096
 
   touch .ssl-created
 fi
