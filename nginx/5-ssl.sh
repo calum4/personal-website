d7f17156efc35878de0e7c68bb3c3a005512ae6b
Calum <calum@calumm.dev>
2025-08-27 20:37:55 +0100

    fix: generate dhparam if bringing your own certs

--- nginx/5-ssl.sh
+++ nginx/5-ssl.sh
@@ -5,5 +5,9 @@ set -e
 if [ ! -e /etc/ssl/personal-website/personal-website.key ] || [ ! -e /etc/ssl/personal-website/personal-website.crt ]
 then
     openssl req -x509 -nodes -days 5475 -newkey rsa:2048 -keyout /etc/ssl/personal-website/personal-website.key -out /etc/ssl/personal-website/personal-website.crt -batch
+fi
+
+if [ ! -e /etc/ssl/personal-website/dhparam.pem ]
+then
     openssl dhparam -dsaparam -out /etc/ssl/personal-website/dhparam.pem 4096
 fi
